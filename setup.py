c2e3733317ba7a0b249a0a4f2f59a8bd94f67b7a
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-07-26 09:40:38 +0800

    RELEASE : Bumped version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*parts):
     return codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts), 'r').read()
 
 setup(name="hitch",
-      version="0.4.1",
+      version="0.4.2",
       description="Loosely coupled testing framework",
       long_description=read('README.rst'),
       classifiers=[
