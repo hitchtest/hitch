0dfdec6d9ca6c8e66b2d3578be2b186e86dc2cd6
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-10-24 11:59:59 +0800

    FEATURE : Clearer error messages for dealing with virtualenv and python.

--- hitch/languagestrings.py
+++ hitch/languagestrings.py
@@ -11,36 +11,61 @@ Create hitch virtualenv using specific virtualenv
 YOU_MUST_HAVE_VIRTUALENV_INSTALLED = """\
 You must have virtualenv installed to use hitch.
 
-To install:
+Suggestions:
+
+#1 Install via your system's package manager:
   - On Ubuntu/Debian : sudo apt-get install python-virtualenv
   - On Fedora        : sudo yum install python-virtualenv
   - On Arch          : sudo pacman -Sy python-virtualenv
   - On Mac OS X      : pip install --upgrade virtualenv
+
+#2 Install via pip, e.g.:
+  - sudo pip3 install --upgrade virtualenv
 """
 
 YOU_MUST_HAVE_PYTHON3_INSTALLED = """\
-To use Hitch, you must have python 3 installed on your system
-and available. If your python3 is not on the system path with
-the name python3, specify its exact location using --python.
+To use Hitch, you must have python 3 installed.
 
 To install:
   - On Ubuntu/Debian : sudo apt-get install python3
   - On Fedora        : sudo yum install python3
   - On Arch          : sudo pacman -Sy python3
   - On Mac OS X      : brew install python3
+
+If your python3 is *not* on the system path with the name 'python3',
+you can specify the location of its virtualenv executable like so:
+
+hitch init --virtualenv=/path/to/python3/bin/virtualenv
+
+Or specify the location of the python3 interpreter, e.g.
+
+hitch init --python=/path/to/python3/bin/python3
 """
 
 YOU_MUST_HAVE_VERSION_ABOVE_PYTHON33 = """\
 Hitch must have python 3.3 or higher installed to run.
-Your app can run with earlier versions of python, but the tests can't.
+Your app can run with earlier versions of python, but the
+testing environment cannot.
 
-You may need to run a sytem upgrade or upgrade your OS.
+Suggestions:
+
+#1 You may need to run a sytem upgrade or upgrade your OS.
+
+#2 If you have python 3.3+ installed but it is not accessible
+on the system path with the command 'python3', you can run:
+
+hitch init --virtualenv=/path/to/python3/bin/virtualenv
+
+OR
+
+hitch init --python=/path/to/python3/bin/python3
 """
 
 ERROR_INITIALIZING_HITCH = """\
 \nError initializing hitch. Problem checklist:\n
 * Was there a problem with your internet?
 * Was there a python package being installed that couldn't compile?\n
+
 Try searching for any errors printed above or raising an issue at:
 http://github.com/hitchtest/hitch/issues/
 """
